b8c091f36db334eb7d2fe7a559004270f625fdd7
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2021-07-19 00:05:54 -0400

    section-4

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -44,8 +44,8 @@ public class Student {
 
     @Column(
             name = "email",
-            nullable = false,
-            columnDefinition = "TEXT"
+            nullable = false/*,
+            columnDefinition = "TEXT"*/
     )
     private String email;
 
